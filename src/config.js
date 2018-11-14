ef21dd56182dc7e24854da6113c7701ac63a774e
Kelen <kelen@KelendeMacBook-Pro.local>
2018-11-15 00:13:42 +0800

    add personal page

--- src/config.js
+++ src/config.js
@@ -4,7 +4,8 @@ const host = 'http://localhost:5757'
 
 const config = {
     host,
-    loginUrl: `${host}/weapp/login`
+    loginUrl: `${host}/weapp/login`,
+    personUrl: `${host}/person_images/`
 }
 
 export default config
